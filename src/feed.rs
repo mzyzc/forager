c6f4d8582b1faaa1271ebdf297feb9ccb364bb95
mzyzc <mzyzc@czyz.xyz>
2020-10-26 17:31:10 +0000

    Feed written to stdout

--- src/feed.rs
+++ src/feed.rs
@@ -1,11 +1,20 @@
 use curl::easy::Easy;
+use roxmltree;
 
-pub fn print_feed() {
-    let xml = fetch_xml("https://news.ycombinator.com/rss");
-    println!("{}", xml);
+pub struct FeedItem {
+    pub title: String,
+    pub link: String,
+    pub description: String,
 }
 
-fn fetch_xml(url: &str) -> String {
+// Fetch XML data from a URL
+pub fn print_feed() {
+    let rss = fetch_rss("https://news.ycombinator.com/rss");
+    println!("Fetch successful");
+    parse_rss(&rss);
+}
+
+fn fetch_rss(url: &str) -> String {
     let mut data = Vec::new();
     let mut handle = Easy::new();
     handle.url(url).unwrap();
@@ -22,8 +31,42 @@ fn fetch_xml(url: &str) -> String {
     String::from_utf8_lossy(&data).to_string()
 }
 
-// struct feedItem: title, link, description
-
 // function parse_rss(string) -> [feedItem]
+fn parse_rss(rss: &str) {
+    let doc = roxmltree::Document::parse(rss).unwrap();
+    let mut pointer = doc.root_element();
+    
+    if !pointer.has_tag_name("rss") {
+        panic!("invalid RSS feed: bad 'rss' node")
+    }
 
-// function print_feed(feedItem)
+    pointer = pointer.first_element_child().expect("invalid RSS feed: no child for 'rss' node");
+
+    if !pointer.has_tag_name("channel") {
+        panic!("invalid RSS feed: bad 'channel' node")
+    }
+
+    let items = pointer.children()
+        .filter(|node| node.has_tag_name("item"));
+
+    for item in items {
+        pointer = item;
+
+        if !pointer.has_tag_name("item") {
+            panic!("invalid RSS feed: bad 'item' node")
+        }
+
+        for element in item.children() {
+            pointer = element;
+
+            match pointer.tag_name().name() {
+                "title" => println!("title: {}", pointer.text().unwrap()),
+                "link" => println!("link: {}", pointer.text().unwrap()),
+                "description" => println!("description: {}", pointer.text().unwrap()),
+                _ => (),
+            }
+        }
+
+        println!();
+    }
+}
